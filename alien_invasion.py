a3e418fd94b786d82e3bb5b7bf19010c7e451932
hubhack <2329259856@qq.com>
2019-06-01 22:33:32 +0800

    设置背景色为白色

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption('Alien Invasion')
+    # 设置背景色
+    bg_color = (230, 230, 230)
 
     # 开始游戏的主循环
     while True:
@@ -13,6 +15,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
