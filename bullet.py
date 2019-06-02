a8609d8c47a1f886607e1c8b7c56e564c2fd695f
hubhack <2329259856@qq.com>
2019-06-02 19:34:02 +0800

    add

--- bullet.py
+++ bullet.py
@@ -4,7 +4,7 @@ class Bullet(Sprite):
     '''一个飞船反射的子弹进行管理的类'''
     def __init__(self, ai_settings, screen, ship):
         '''在飞船所在处的位置创建一个子弹对象'''
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
@@ -16,8 +16,10 @@ class Bullet(Sprite):
         self.speed_factor = ai_settings.bullet_speed_factor
 
     def updata(self):
-        self.y -=self.speed_factor
+        '''向上移动子弹'''
+        self.y -= self.speed_factor
         self.rect.y = self.y
 
     def draw_buddet(self):
+        '''在屏幕上绘制子弹'''
         pygame.draw.rect(self.screen, self.color, self.rect)
